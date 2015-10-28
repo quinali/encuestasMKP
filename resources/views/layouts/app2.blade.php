e03ef5ca7fffca60bf41ed7d94f41930c4e691e7
quinali <quinali@gmail.com>
2015-10-28 15:28:13 +0100

    Finalizada nueva zona de admon

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -59,7 +59,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">MKPersons</a>
+                <a class="navbar-brand" href="{{route('admin')}}" > MKPersons</a>
             </div>
            
             <!-- Top Menu Items -->
